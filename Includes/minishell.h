734213c094c94c10a789f4e5641e804dfa3bc853
Violeta desde 42 <avelandr@cbr8s5.42barcelona.com>
2025-09-12 17:18:31 +0200

    he importado las librerias

--- Includes/minishell.h
+++ Includes/minishell.h
@@ -5,6 +5,20 @@
 
 //Includes de las librerias que aparecen en el readme
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <signal.h>
+#include <dirent.h>
+#include <termios.h>
+#include <termcap.h>
+
 //Definiciones de estructuras de datos y typedefs utilizados
 
 //Definiciones de funciones del proyecto
