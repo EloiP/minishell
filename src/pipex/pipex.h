a220ea801fdd6c8df084d6bd11b5bd050c6ab169
Eloy Antoni Pascual Perez-Casaux <epascual@cbr15s2.42barcelona.com>
2025-09-22 11:46:48 +0200

    Anadido lo principal de pipex.

---
+++ src/pipex/pipex.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epascual <epascual@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/11 16:40:32 by epascual          #+#    #+#             */
+/*   Updated: 2025/04/29 14:56:23 by epascual         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <string.h>
+# include <errno.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include "../../libft/Includes/libft.h"
+
+typedef signed int	t_pid;
+char	*getenvar(char *name, char **env);
+void	cierra(int in, int out);
+void	finish(int error);
+void	ft_free_pointstring(char **tab);
+void	doblefree(char **a, char **b);
+int		checkacces(char **s_cmd, char **allpath, char **exec);
+int		notempty(char **a, char **b);
+#endif
