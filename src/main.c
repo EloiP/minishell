46528e7cdae971f91846bf515306a63d58538350
Violeta desde 42 <avelandr@cbr8s5.42barcelona.com>
2025-09-22 13:36:56 +0200

    eloy g(u)ay

--- src/main.c
+++ src/main.c
@@ -5,15 +5,12 @@ int main(int argc, char **argv, char **env)
 	t_data	data;
 
 	ft_memset(&data, 0, sizeof(t_data));
-	// parseo
 	if (!check_execution(&data, argc, argv))
 		exit_shelly();
-	// interactive
 	if (data.is_interactive)
 		exec_interactive(&data);
 	else
 		exect_noninteractive(&data);
-	// libera y termina
 	exit_shelly(&data);
 	return (0);
 }
