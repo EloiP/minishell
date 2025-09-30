ef1809a0e646324ab634cea2726d6ca8bdff4c3f
Violeta desde 42 <avelandr@cbr8s5.42barcelona.com>
2025-09-30 19:06:48 +0200

    aaaaaaaa

--- src/main.c
+++ src/main.c
@@ -4,13 +4,14 @@ int main(int argc, char **argv, char **env)
 {
 	t_data	data;
 
+	(void)env;
 	ft_memset(&data, 0, sizeof(t_data));
 	if (!check_execution(&data, argc, argv))
 		exit_shelly();
 	if (data.is_interactive)
-		exec_interactive(&data);
+		exect_interactive(&data);
 	else
-		exect_noninteractive(&data, argc, argv//temporal, hasta que rellenemos el data en checkexecution
+		exect_noninteractive(&data, argc, argv);//temporal, hasta que rellenemos el data en checkexecution
 	exit_shelly(&data);
 	return (0);
 }
